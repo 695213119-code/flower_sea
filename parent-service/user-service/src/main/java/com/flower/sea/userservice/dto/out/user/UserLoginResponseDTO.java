ace53cbe25238791bb9e43cd29bef01aa8eb1e8b
zhanglei <18827529732@163.com>
2020-01-11 21:04:26 +0800

    update 修改jackson的版本

--- parent-service/user-service/src/main/java/com/flower/sea/userservice/dto/out/user/UserLoginResponseDTO.java
+++ parent-service/user-service/src/main/java/com/flower/sea/userservice/dto/out/user/UserLoginResponseDTO.java
@@ -15,24 +15,4 @@ public class UserLoginResponseDTO {
     @ApiModelProperty("用户token")
     private String userToken;
 
-//    @ApiModelProperty(value = "用户昵称")
-//    private String nickName;
-//
-//    @ApiModelProperty(value = "用户头像")
-//    private String avatar;
-//
-//    @ApiModelProperty(value = "用户年龄")
-//    private Integer age;
-//
-//    @ApiModelProperty(value = "用户性别 1-男 2-女 0-未知")
-//    private Integer gender;
-//
-//    @ApiModelProperty(value = "用户生日(农历)")
-//    @JsonFormat(pattern="yyyy-MM-dd")
-//    private LocalDateTime birth;
-//
-//    @ApiModelProperty(value = "用户下一次的生日时间(阳历)")
-//    @JsonFormat(pattern="yyyy-MM-dd")
-//    private LocalDateTime birthAnother;
-
 }
